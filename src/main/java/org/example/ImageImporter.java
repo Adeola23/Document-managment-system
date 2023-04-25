1f09c5edbec34c19e0814bf782df8706d92ce40a
Adeola23 <amosadeola92@gmail.com>
2023-04-25 20:15:32 +0100

    attribute class for the different keys

--- src/main/java/org/example/ImageImporter.java
+++ src/main/java/org/example/ImageImporter.java
@@ -8,19 +8,19 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.example.Attriibutes.*;
+
 public class ImageImporter implements Importer{
 
     @Override
     public  Document importFile(final File file) throws IOException{
         final Map<String, String> attributes = new HashMap<>();
-        attributes.put("PATH", file.getPath());
+        attributes.put(PATH, file.getPath());
         final BufferedImage image = ImageIO.read(file);
-        attributes.put("WIDTH", String.valueOf(image.getWidth()));
-        attributes.put("HEIGHT", String.valueOf(image.getHeight()));
-        attributes.put("TYPE", "IMAGE");
+        attributes.put(WIDTH, String.valueOf(image.getWidth()));
+        attributes.put(HEIGHT, String.valueOf(image.getHeight()));
+        attributes.put(TYPE, "IMAGE");
         return new Document(attributes);
-
-
 
     }
 }
