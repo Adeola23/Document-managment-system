300e88e25b6f583f390f82ff04099eaa435a0f9c
Adeola23 <amosadeola92@gmail.com>
2023-04-27 22:47:20 +0100

    Text file

---
+++ src/main/java/org/example/TextFile.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+import java.util.Map;
+
+public class TextFile {
+    private final Map<String, String> attributes;
+    private final List<String> lines;
+
+}
