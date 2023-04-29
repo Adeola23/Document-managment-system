9d60b6625f9558c35b5f74ba1a0bc0691d6f14cc
Adeola23 <amosadeola92@gmail.com>
2023-04-29 23:25:30 +0100

    add lines method

--- src/main/java/org/example/LetterImporter.java
+++ src/main/java/org/example/LetterImporter.java
@@ -1,4 +1,22 @@
 package org.example;
 
-public class LetterImporter {
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.example.Attriibutes.*;
+
+public class LetterImporter implements Importer {
+
+    private static final String NAME_PREFIX = "Dear ";
+    private static final String AMOUNT_PREFIX = "Amount:";
+    @Override
+    public Document importFile (final File file) throws IOException {
+        final TextFile textFile = new TextFile(file);
+        textFile.addLineSuffix(NAME_PREFIX, PATIENT);
+
+        final int lineNumber = textFile.addLineSuffix();
+
+    }
 }
--- src/main/java/org/example/TextFile.java
+++ src/main/java/org/example/TextFile.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.example.Attriibutes.*;
@@ -31,6 +32,25 @@ public class TextFile {
                 break;
             }
         }
+    }
+
+    int addLines(
+            final int start,
+            final Predicate<String> isEnd,
+            final String attributeName
+    ){
+        final StringBuilder accumulator = new StringBuilder();
+        int lineNumber;
+        for(lineNumber = start; lineNumber<lines.size(); lineNumber++){
+            final String line = lines.get(lineNumber);
+            if(isEnd.test(line)){
+                break;
+            }
+            accumulator.append(line);
+            accumulator.append("\n");
+        }
+        attributes.put(attributeName, accumulator.toString().trim());
+        return lineNumber;
     }
 
 }
