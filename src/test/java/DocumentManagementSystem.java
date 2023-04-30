8bb6986f9e1c7ebc344d887d154dc5c98b24639d
Adeola23 <amosadeola92@gmail.com>
2023-04-30 23:18:14 +0100

    should import test

---
+++ src/test/java/DocumentManagementSystem.java
@@ -0,0 +1,27 @@
+import org.example.Attriibutes;
+import org.example.Document;
+import org.example.DocumentManagementSystem;
+import org.junit.Test;
+
+import java.io.File;
+
+
+public class DocumentManagementSystemTest {
+    private static final String RESOURCES =
+            "src" + File.separator + "test" + File.separator + "resources" + File.separator;
+    private static final String LETTER = RESOURCES + "patient.letter";
+    private static final String REPORT = RESOURCES + "patient.report";
+    private static final String XRAY = RESOURCES + "xray.jpg";
+    private static final String INVOICE = RESOURCES + "patient.invoice";
+    private static final String JOE_BLOGGS = "Joe Bloggs";
+    private DocumentManagementSystem system = new DocumentManagementSystem();
+
+    @Test
+    public void shouldImportFile() throws Exception{
+        system.importFile(LETTER);
+        final Document document = onlyDocument();
+        assertAttributeEquals(document, Attriibutes.PATH, LETTER);
+    }
+
+
+}
